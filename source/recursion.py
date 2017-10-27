1f68558e59d1bbcfa2d8b8efa8d49146ddc1442e
Sam Galizia <samgalizia@gmail.com>
2017-10-27 10:44:13 -0700

    Updated factorial_iterative

--- source/recursion.py
+++ source/recursion.py
@@ -14,13 +14,10 @@ def factorial(n):
 
 
 def factorial_iterative(n):
-    if n == 0 or n == 1:
-        return 1
-    elif n > 1:
-        result = 1
-        for i in range(1, n + 1):
-            result *= i
-        return result
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
 
 
 def factorial_recursive(n):
